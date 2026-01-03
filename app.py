2c6d68bc5ab61feacda887d154f4ac1d3dd74ce3
sevvalazrakocak-code <sevvalazrakocak@gmail.com>
2026-01-03 14:11:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import psycopg2
 app = Flask(__name__)
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://odev_veritabani_user:vi7K6Sf6pee90Lr1L7IWSyxtckkCxLmF@dpg-d5cfd7a4d50c73fv7b50-a/odev_veritabani")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://odev_veritabani_user:vi7K6Sf6pee90Lr1L7IWSyxtckkCxLmF@dpg-d5cfd7a4d50c73fv7b50-a.frankfurt-postgres.render.com/odev_veritabani")
 
 HTML = """
 <!doctype html>
